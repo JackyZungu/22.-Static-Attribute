931d09e5cdd7f656de21621360df088796d30503
Lenovo-User <Lenovo-User@DESKTOP-Q92463H>
2023-10-10 12:24:45 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _22.Static_Attribute
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Songs holiday = new Songs("Holiday", "Green Day", 4);
+            Songs kashmir = new Songs("Kashmir", "Led Zeppelin", 5);
+
+            Console.WriteLine(Songs.songCount);
+            Console.ReadLine(); 
+           
+        }
+    }
+}
